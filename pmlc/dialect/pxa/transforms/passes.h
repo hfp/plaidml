9d27193d83bb92f9e441571ee5a1d87b964c4d77
Sanket Tavarageri <sanket.tavarageri@intel.com>
2020-08-26 03:23:43 -0700

    WIP: generating pxa.brgemm through stenciling

--- pmlc/dialect/pxa/transforms/passes.h
+++ pmlc/dialect/pxa/transforms/passes.h
@@ -10,51 +10,54 @@
 
 namespace pmlc::dialect::pxa {
 
-struct StencilCost {
-  double throughput;
-  unsigned startupCost;
-};
+  struct StencilCost {
+    double throughput;
+    unsigned startupCost;
+  };
 
-using StencilCostFunction = std::function<StencilCost(llvm::ArrayRef<int64_t>)>;
+  using StencilCostFunction = std::function<StencilCost(llvm::ArrayRef<int64_t>)>;
 
-std::unique_ptr<mlir::Pass> createAffineNormalizePass();
-std::unique_ptr<mlir::Pass> createAffineNormalizePass(bool promote);
+  std::unique_ptr<mlir::Pass> createAffineNormalizePass();
+  std::unique_ptr<mlir::Pass> createAffineNormalizePass(bool promote);
 
-std::unique_ptr<mlir::Pass> createAutoTileExamplePass();
+  std::unique_ptr<mlir::Pass> createAutoTileExamplePass();
 
-std::unique_ptr<mlir::Pass> createBufferPlacementPass();
+  std::unique_ptr<mlir::Pass> createBufferPlacementPass();
 
-std::unique_ptr<mlir::Pass>
-createFusionPass(int64_t memoryActivityThreshold = 0);
+  std::unique_ptr<mlir::Pass>
+    createFusionPass(int64_t memoryActivityThreshold = 0);
 
-std::unique_ptr<mlir::Pass> createGPUThreadPass();
-std::unique_ptr<mlir::Pass> createGPUThreadPass(unsigned maxThreads);
+  std::unique_ptr<mlir::Pass> createGPUThreadPass();
+  std::unique_ptr<mlir::Pass> createGPUThreadPass(unsigned maxThreads);
 
-std::unique_ptr<mlir::Pass> createLocalizePass();
+  std::unique_ptr<mlir::Pass> createLocalizePass();
 
-std::unique_ptr<mlir::Pass> createMemRefDataFlowOptPass();
+  std::unique_ptr<mlir::Pass> createMemRefDataFlowOptPass();
 
-std::unique_ptr<mlir::Pass> createNestLoopsPass();
-std::unique_ptr<mlir::Pass> createNestLoopsPass(unsigned minLoopIVs);
+  std::unique_ptr<mlir::Pass> createNestLoopsPass();
+  std::unique_ptr<mlir::Pass> createNestLoopsPass(unsigned minLoopIVs);
 
-std::unique_ptr<mlir::Pass> createResizeTmpsPass();
+  std::unique_ptr<mlir::Pass> createResizeTmpsPass();
 
-std::unique_ptr<mlir::Pass> createStencilGEMMPass(unsigned numThreads,
-                                                  StencilCostFunction costFn);
+  std::unique_ptr<mlir::Pass> createStencilGEMMPass(unsigned numThreads,
+    StencilCostFunction costFn);
 
-std::unique_ptr<mlir::Pass> createSubgroupsPass();
+  std::unique_ptr<mlir::Pass> createStencilBRGEMMPass(unsigned numThreads,
+    StencilCostFunction costFn);
 
-std::unique_ptr<mlir::Pass> createTestStrideInfoPass();
+  std::unique_ptr<mlir::Pass> createSubgroupsPass();
 
-std::unique_ptr<mlir::Pass> createTestIndirectUsesIteratorPass();
+  std::unique_ptr<mlir::Pass> createTestStrideInfoPass();
 
-std::unique_ptr<mlir::Pass> createTestIndirectValuesIteratorPass();
+  std::unique_ptr<mlir::Pass> createTestIndirectUsesIteratorPass();
 
-std::unique_ptr<mlir::Pass> createTileAccumulatePass();
+  std::unique_ptr<mlir::Pass> createTestIndirectValuesIteratorPass();
 
-std::unique_ptr<mlir::Pass> createVectorizeExamplePass();
+  std::unique_ptr<mlir::Pass> createTileAccumulatePass();
 
-/// Generate the code for registering passes.
+  std::unique_ptr<mlir::Pass> createVectorizeExamplePass();
+
+  /// Generate the code for registering passes.
 #define GEN_PASS_REGISTRATION
 #include "pmlc/dialect/pxa/transforms/passes.h.inc"
 
